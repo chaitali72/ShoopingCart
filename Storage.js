9edd6deb383d8a5fc9b4c89cf8bf1e32549d8a4e
Chaitali <60978542+chaitali72@users.noreply.github.com>
2020-08-14 14:53:49 -0400

    Add files via upload

---
+++ Storage.js
@@ -0,0 +1,10 @@
+//local storage
+class Storage {
+  static saveProduct(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find(product => product.id === id);
+  }
+}
